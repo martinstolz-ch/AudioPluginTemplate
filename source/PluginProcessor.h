c56e452f766a10b30fd8ff1940a2c0c449ed9c54
Martin Stolz <info@martinstolz.ch>
2025-09-26 08:59:46 +0200

    Update PluginProcessor.h

--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -9,7 +9,6 @@
 #include "binaryAssets.h"
 #include "cmakeVar.h"
 #include "config.h"
-#include "Sf2Engine.h"
 
 namespace pep {
 
@@ -34,74 +33,18 @@ public:
 
     // DSP stuff
     void prepareToPlay (const double sampleRate, const int samplesPerBlock) override {
-
-        // load sound font and select preset
-        _sf2Engine.loadSf2Binary (binaryAssets::galaxy_sf2, binaryAssets::galaxy_sf2Size);
-        _sf2Engine.selectPreset (0, 5);
-
-        // init render buffer and audio engine
-        _sf2Engine.setRenderBuffer (static_cast <size_t> (samplesPerBlock) * 4); // take 4x 4 enough space ;)
-        _sf2Engine.setOutput    (config::audio::numChannelsExpected, sampleRate);
-        _sf2Engine.setMaxVoices (config::synth::maxVoices);
-        _sf2Engine.setTuning    (config::synth::defaultTuningHz);
     }
 
     void releaseResources() override {
-        _sf2Engine.unloadSf2();
     }
 
-    void processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages) override
+    void processBlock (AudioBuffer<float>& buffer, MidiBuffer& /* midiMessages */) override
     {
         ScopedNoDenormals noDenormals;
 
         const int totalNumSamples = buffer.getNumSamples();
         buffer.clear();
-
-        int startSample = 0;
-
-        for (const auto metadata : midiMessages)
-        {
-            const auto& msg     = metadata.getMessage();
-            const int samplePos = metadata.samplePosition;
-
-            // Länge bis zu diesem Event
-            int numSamples = samplePos - startSample;
-            if (numSamples > 0)
-            {
-                AudioSourceChannelInfo info (&buffer, startSample, numSamples);
-                _sf2Engine.renderAudio (info);
-                startSample += numSamples;
-            }
-
-            if (msg.isNoteOn())
-                _sf2Engine.noteOn (0, msg.getNoteNumber(), msg.getVelocity());
-
-            else if (msg.isNoteOff())
-                _sf2Engine.noteOff (0, msg.getNoteNumber());
-
-            else if (msg.isController())
-            {
-                const int controller = msg.getControllerNumber();
-                const int value      = msg.getControllerValue();
-                _sf2Engine.midiController(0, controller, value);
-            }
-
-            else if (msg.isPitchWheel())
-            {
-                int value14bit = msg.getPitchWheelValue(); // JUCE liefert 0–16383, Mitte=8192
-                _sf2Engine.pitchWheel (0, value14bit);
-            }
-        }
-
-        // Rest des Blocks rendern
-        const int remaining = totalNumSamples - startSample;
-        if (remaining > 0)
-        {
-            AudioSourceChannelInfo info (&buffer, startSample, remaining);
-            _sf2Engine.renderAudio (info);
-        }
     }
-
 
     // system stuff
     AudioProcessorEditor* createEditor() override;
@@ -123,7 +66,7 @@ public:
     void setStateInformation (const void*, int) override {}
 
 private:
-    Sf2Engine _sf2Engine;
+    
 };
 
 }
